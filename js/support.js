9b41567492582fd141326e70b043e439edb2ebe6
Alejandro Meza <vanzcats@hotmail.com>
2021-07-11 20:09:35 -0700

    added js functionality

---
+++ js/support.js
@@ -0,0 +1,5 @@
+export const name = "Alejandro Meza"
+
+export function sayName(name) {
+    return `My name is ${name}`
+}
--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+import { name, sayName } from './js/support.js'
+
 console.log('This JS script is running!')
+
+console.log(`Importing variable from another js: ${name}`)
+
+console.log(sayName('Meza Zamora'))
