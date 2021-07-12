1ec6d23874edfc7f1c49f69b55d9ec9b56bbcb49
Alejandro Meza <vanzcats@hotmail.com>
2021-07-11 21:12:00 -0700

    Added more functionality to js files

--- js/support.js
+++ js/support.js
@@ -3,3 +3,18 @@ export const name = "Alejandro Meza"
 export function sayName(name) {
     return `My name is ${name}`
 }
+
+
+export function loadNames(names){
+    const dynamicContent = document.querySelector('#dynamic-content')
+
+    dynamicContent.innerHTML = ''
+
+    const df = new DocumentFragment();
+    names.forEach(name => {
+        let p = document.createElement('P')
+        p.textContent = name
+        df.appendChild(p)
+    })
+    dynamicContent.appendChild(df)
+}
--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-import { name, sayName } from './js/support.js'
+import { name, sayName, loadNames } from './js/support.js'
 
 console.log('This JS script is running!')
 
 console.log(`Importing variable from another js: ${name}`)
 
 console.log(sayName('Meza Zamora'))
+
+const button = document.querySelector('#js-tryout');
+
+const people = [
+    {name: "Alejandro", 
+    age: 25,
+    email: "alex@gmail.com"
+    },
+    {name: "Eva", 
+    age: 22,
+    email: "Eva@gmail.com"
+    },
+    {name: "Vero", 
+    age: 24,
+    email: "vero@outlook.com"
+    }
+]
+
+const names = [...people].map(person => {
+    return person.name
+})
+
+console.log(names)
+
+button.addEventListener('click', () => {
+    loadNames(names)
+})
